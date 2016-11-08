72849aa32f54adf8b6d6ef219a0dd06a9cd27b0a
GreatBullet <rick.armbruster@hotmail.de>
2016-11-08 17:45:56 +0100

    Add project files.

---
+++ Project11/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int var1 = 10;
+	int var2 = 133;
+	bool result;
+	result = var1 == var2 ? true : false;
+	cout << result << endl;
+	system("pause");
+	return 0;
+}
